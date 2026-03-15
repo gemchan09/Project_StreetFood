a01a634f090563ac6861b99250160331af41e4fd
gemchan <minhngoc25318@gmail.com>
2026-03-16 01:44:46 +0700

    Add files via upload

---
+++ src/TourGuide.Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using TouristGuide.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=touristguide.db"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Auto-migrate
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
+app.Run("http://0.0.0.0:5000");
